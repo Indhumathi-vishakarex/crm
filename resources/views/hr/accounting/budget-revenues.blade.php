5601f9c40899687109ee1e5a0615442f216f0a82
janarth24 <janarthanan@vishakarex.in>
2025-07-09 19:45:16 +0530

    title and logo change

--- resources/views/hr/accounting/budget-revenues.blade.php
+++ resources/views/hr/accounting/budget-revenues.blade.php
@@ -11,8 +11,9 @@
   <meta name="description" content="Smarthr - Bootstrap Admin Template" />
   <meta name="keywords" content="admin, estimates, bootstrap, business, corporate, creative, management, minimal, modern, accounts, invoice, html5, responsive, CRM, Projects" />
   <meta name="author" content="Dreamstechnologies - Bootstrap Admin Template" />
-  <title>Categories - HRMS admin template</title>
-  <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png" />
+  <title>CRM - Winngoopages</title>
+  <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/logo2.png') }}">
+
   <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
   <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css" />
   <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css" />
